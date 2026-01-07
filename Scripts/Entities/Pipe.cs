e160d91f966dd0fcc3228c9c5b3b16f8ba20451e
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-07 09:11:05 +0100

    Add scrolling, collision, and pass detection logic to Pipe.cs

--- Scripts/Entities/Pipe.cs
+++ Scripts/Entities/Pipe.cs
@@ -1,6 +1,49 @@
+using Godot;
+
 namespace Flappy_Bird.Scripts.Entities;
 
-public class Pipe
+public partial class Pipe : Area2D
 {
-    
+	private const float ScrollSpeed = 150.0f;
+	private const float DestroyPositionX = -200.0f;
+	private bool _hasBeenPassed = false;
+
+	[Signal]
+	public delegate void PipePassedEventHandler();
+
+	public override void _Ready()
+	{
+		BodyEntered += OnBodyEntered;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Position += new Vector2(-ScrollSpeed * (float)delta, 0);
+
+		if (!_hasBeenPassed && Position.X < 100)
+		{
+			_hasBeenPassed = true;
+			EmitSignal(SignalName.PipePassed);
+		}
+
+		if (Position.X < DestroyPositionX)
+		{
+			QueueFree();
+		}
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is Bird bird)
+		{
+			bird.Kill();
+		}
+	}
+
+	public void SetGapPosition(float gapY)
+	{
+		Position = new Vector2(Position.X, gapY);
+	}
+
+	public bool HasBeenPassed() => _hasBeenPassed;
 }
