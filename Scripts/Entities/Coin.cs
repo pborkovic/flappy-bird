ed0634af709466f0e86c46fc34f9345f3b20fd68
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-22 17:36:41 +0100

    Convert `Coin` to `Area2D`, add scrolling logic, collection handling, and signaling for `CoinCollected`.

--- Scripts/Entities/Coin.cs
+++ Scripts/Entities/Coin.cs
@@ -1,6 +1,41 @@
+using Godot;
+
 namespace Flappy_Bird.Scripts.Entities;
 
-public class Coin
+public partial class Coin : Area2D
 {
-    
+	public static float ScrollSpeed = 150.0f;
+	private const float DestroyPositionX = -100.0f;
+	private bool _isCollected = false;
+
+	[Signal]
+	public delegate void CoinCollectedEventHandler();
+
+	public override void _Ready()
+	{
+		BodyEntered += OnBodyEntered;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Position += new Vector2(-ScrollSpeed * (float)delta, 0);
+
+		if (Position.X < DestroyPositionX)
+		{
+			QueueFree();
+		}
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (_isCollected)
+			return;
+
+		if (body is Bird)
+		{
+			_isCollected = true;
+			EmitSignal(SignalName.CoinCollected);
+			QueueFree();
+		}
+	}
 }
