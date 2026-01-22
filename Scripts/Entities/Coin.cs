5104b4c0b92d8154e69d8bf70733e228b9cbfe48
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-22 19:27:57 +0100

    Add bouncing behavior to Coin entity using sine wave offset for vertical motion

--- Scripts/Entities/Coin.cs
+++ Scripts/Entities/Coin.cs
@@ -6,7 +6,11 @@ public partial class Coin : Area2D
 {
 	public static float ScrollSpeed = 150.0f;
 	private const float DestroyPositionX = -100.0f;
+	private const float BounceAmount = 3.0f;
+	private const float BounceSpeed = 5.0f;
 	private bool _isCollected = false;
+	private float _time = 0.0f;
+	private float _baseY;
 
 	[Signal]
 	public delegate void CoinCollectedEventHandler();
@@ -14,11 +18,18 @@ public partial class Coin : Area2D
 	public override void _Ready()
 	{
 		BodyEntered += OnBodyEntered;
+		_baseY = Position.Y;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Position += new Vector2(-ScrollSpeed * (float)delta, 0);
+		_time += (float)delta;
+		float bounceOffset = Mathf.Sin(_time * BounceSpeed) * BounceAmount;
+
+		Position = new Vector2(
+			Position.X - ScrollSpeed * (float)delta,
+			_baseY + bounceOffset
+		);
 
 		if (Position.X < DestroyPositionX)
 		{
