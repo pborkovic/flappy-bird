55b36a436e5b9b08c38cf49d510232d94573ee19
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-22 19:25:20 +0100

    Handle bird collisions more reliably by tracking `_hasHitBird` across pipe updates and ensuring single collision processing

--- Scripts/Entities/Pipe.cs
+++ Scripts/Entities/Pipe.cs
@@ -7,6 +7,7 @@ public partial class Pipe : Area2D
 	public static float ScrollSpeed = 150.0f;
 	private const float DestroyPositionX = -200.0f;
 	private bool _hasBeenPassed = false;
+	private bool _hasHitBird = false;
 
 	[Signal]
 	public delegate void PipePassedEventHandler();
@@ -14,11 +15,26 @@ public partial class Pipe : Area2D
 	public override void _Ready()
 	{
 		BodyEntered += OnBodyEntered;
+		Monitoring = true;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Position += new Vector2(-ScrollSpeed * (float)delta, 0);
+
+		if (!_hasHitBird && HasOverlappingBodies())
+		{
+			foreach (Node2D body in GetOverlappingBodies())
+			{
+				if (body is Bird bird && bird.IsAlive())
+				{
+					_hasHitBird = true;
+					bird.Kill();
+					
+					break;
+				}
+			}
+		}
 
 		if (!_hasBeenPassed && Position.X < 100)
 		{
@@ -34,8 +50,9 @@ public partial class Pipe : Area2D
 
 	private void OnBodyEntered(Node2D body)
 	{
-		if (body is Bird bird)
+		if (!_hasHitBird && body is Bird bird && bird.IsAlive())
 		{
+			_hasHitBird = true;
 			bird.Kill();
 		}
 	}
